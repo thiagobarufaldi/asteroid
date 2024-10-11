144913ff58145b273ceaa347165e0547bb7b5ef9
thiagobarufaldi <39417670+thiagobarufaldi@users.noreply.github.com>
2024-10-11 10:09:23 -0300

    Player added

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    fps = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = fps.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
