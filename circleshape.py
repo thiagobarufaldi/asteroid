95ee65e762c3b6e80700ef1b7dd2b725bef101c3
thiagobarufaldi <39417670+thiagobarufaldi@users.noreply.github.com>
2024-10-11 13:02:56 -0300

    Shots added

--- circleshape.py
+++ circleshape.py
@@ -20,4 +20,5 @@ class CircleShape(pygame.sprite.Sprite):
         # sub-classes must override
         pass
 
-
+    def collides_with(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
